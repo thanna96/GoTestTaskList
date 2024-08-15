5f075d965afe66e93317c6bcb9ec97c5d1fb0195
thomashanna <thanna96@gmail.com>
2024-08-15 16:43:56 -0400

    Updated theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,20 @@
-import { Container, Stack} from "@chakra-ui/react";
-import Navbar from "./components/Navbar.tsx";
-import TodoForm from "./components/TodoForm.tsx";
-import TodoList from "./components/TodoList.tsx";
+import { Container, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
-
-  return (
-    <Stack h="100vh">
-        <Navbar/>
-        <Container>
-            <TodoForm/>
-            <TodoList/>
-        </Container>
-    </Stack>
-  )
+    return (
+        <Stack h='100vh'>
+            <Navbar />
+            <Container>
+                <TodoForm />
+                <TodoList />
+            </Container>
+        </Stack>
+    );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {ChakraProvider} from "@chakra-ui/react";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./chakra/theme.ts";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-          <App />
-      </ChakraProvider>
-      </QueryClientProvider>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <App />
+            </ChakraProvider>
+        </QueryClientProvider>
+    </React.StrictMode>
+);
